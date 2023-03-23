a56930f392a620a91ddecf4ea7abb78e8bfb79c8
evansutanto <evansutanto54@gmail.com>
2023-03-23 16:16:44 +0100

    Added vitals panel

--- src/main/java/softwaredesign/HelloApplication.java
+++ src/main/java/softwaredesign/HelloApplication.java
@@ -2,10 +2,14 @@ package softwaredesign;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
@@ -21,7 +25,6 @@ public class HelloApplication extends Application {
 //        Should be panel since container
         Label label_center = new Label("this is BorderPane center");
         label_center.setBackground(Background.fill(Color.LIGHTGRAY));
-        label_center.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
         root.setCenter(label_center);
 
         Label label_top = new Label("this is BorderPane top");
@@ -33,8 +36,33 @@ public class HelloApplication extends Application {
         Label label_bottom = new Label("this is BorderPane bottom");
         label_bottom.setBackground(Background.fill(Color.YELLOW));
         label_bottom.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
-        label_bottom.setPrefHeight(180);
-        root.setBottom(label_bottom);
+
+        HBox allVitals = new HBox();
+        Vital hunger = new Vital();
+        Vital sleepiness = new Vital();
+        Vital hygiene = new Vital();
+        Vital mood = new Vital();
+
+        allVitals.getChildren().add(hunger.render());
+        allVitals.getChildren().add(sleepiness.render());
+        allVitals.getChildren().add(hygiene.render());
+        allVitals.getChildren().add(mood.render());
+        allVitals.setAlignment(Pos.CENTER_RIGHT);
+
+
+        TilePane tile = new TilePane();
+        tile.setPadding(new Insets(10, 10, 10, 10));
+        tile.setPrefColumns(2);
+        tile.setStyle("-fx-background-color: #CD5C5C;");
+        Label top = new Label("Top");
+        Label left = new Label("Left");
+        Label center = new Label("Center");
+
+
+        tile.getChildren().add(center);
+
+        allVitals.setPrefHeight(180);
+        root.setBottom(allVitals);
 
         Label label_left = new Label("this is BorderPane left");
         label_left.setBackground(Background.fill(Color.LIGHTGREEN));
