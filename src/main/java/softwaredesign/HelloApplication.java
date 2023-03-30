d4bbfdea07146ff2fa1fd3123e1193353f646823
alvaroprtm <alvaropmaharto@gmail.com>
2023-03-30 17:07:11 +0200

    Added fade transition, title page, and game over state.

--- src/main/java/softwaredesign/HelloApplication.java
+++ src/main/java/softwaredesign/HelloApplication.java
@@ -1,18 +1,20 @@
 package softwaredesign;
 
+import javafx.animation.FadeTransition;
+import javafx.animation.PauseTransition;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.control.Label;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
-import javafx.scene.text.Text;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 import java.io.IOException;
 
 
@@ -24,8 +26,28 @@ public class HelloApplication extends Application {
 //        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
 //        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
 
-        startCustomizePage();
-        initCustomPanel();
+        titleState();
+
+        PauseTransition delay = new PauseTransition(Duration.seconds(3));
+        FadeTransition fade = new FadeTransition(Duration.seconds(1), root);
+
+        // Create a delay transition for the titleState
+//        delay.setOnFinished(e -> fade.play());
+//        delay.play();
+
+        // Create a fade transition for the BorderPane
+//        fade.setFromValue(1.0);
+//        fade.setToValue(0.0);
+//        fade.setCycleCount(1);
+//        fade.setOnFinished(e -> {
+//            FadeTransition fade2 = new FadeTransition(Duration.seconds(1.5), root);
+//            fade2.setFromValue(0.0);
+//            fade2.setToValue(1.0);
+//            fade2.play();
+            startCustomizePage();
+//        });
+
+//        gameOverState();
 
         Scene scene = new Scene(root);
         stage.setTitle("Jailbird!");
@@ -33,9 +55,6 @@ public class HelloApplication extends Application {
         stage.setHeight(600);
         stage.setScene(scene);
         stage.show();
-
-
-
     }
 
     public void startCustomizePage() {
@@ -63,7 +82,66 @@ public class HelloApplication extends Application {
     public void initCustomPanel() {
 
     }
+    public Label authorIdentity(String name){
+        Label nameLabel = new Label(name);
+        nameLabel.setTextFill(Color.WHITE);
+        Font font = new Font("Comic Sans MS", 14);
+        nameLabel.setFont(font);
+        return nameLabel;
+    }
+    public void titleState() {
+        ImageView titleImageView = new ImageView(new Image(getClass().getResource("JailBirdTitle.png").toExternalForm()));
+        titleImageView.setFitWidth(600);
+        titleImageView.setFitHeight(300);
+        HBox titleHBox = new HBox(titleImageView);
+        titleHBox.setAlignment(Pos.TOP_CENTER);
 
+        Label groupLabel = authorIdentity("Group 35:");
+        Label TAlabel = authorIdentity("TA: Ariana Vargas Pastor");
+        Label member1 = authorIdentity("Alvaro Pratama Maharto/2734663");
+        Label member2 = authorIdentity("Mahmoud Ashtar/2696767");
+        Label member3 = authorIdentity("Michael Evan Sutanto/2720871");
+        Label member4 = authorIdentity("Miguel Sadorra/2728578");
+
+        VBox membersVBox = new VBox(5);
+        membersVBox.getChildren().addAll(groupLabel, TAlabel, member1, member2, member3, member4);
+        membersVBox.setAlignment(Pos.CENTER);
+
+        VBox titleContainer = new VBox(titleHBox, membersVBox);
+        titleContainer.setAlignment(Pos.CENTER);
+
+        root.setCenter(titleContainer);
+        root.setBackground(new Background(new BackgroundFill(Color.BLACK, null, null)));
+    }
+
+    public void gameOverState(){
+        ImageView gameOverImage = new ImageView(new Image(getClass().getResource("gameOver.png").toExternalForm()));
+        gameOverImage.setFitWidth(400);
+        gameOverImage.setFitHeight(300);
+        HBox imageHBox = new HBox(gameOverImage);
+        imageHBox.setAlignment(Pos.CENTER);
+
+        Button playAgainButton = new Button("Play Again");
+        playAgainButton.setPrefSize(150, 50);
+        playAgainButton.setStyle("-fx-background-color: black");
+        playAgainButton.setFont(new Font("Arial", 20));
+        playAgainButton.setTextFill(Color.WHITE);
+
+        Button exitButton = new Button("Exit");
+        exitButton.setPrefSize(150, 50);
+        exitButton.setStyle("-fx-background-color: black");
+        exitButton.setFont(new Font("Arial", 20));
+        exitButton.setTextFill(Color.WHITE);
+
+        HBox gameOverButtons = new HBox(20, playAgainButton, exitButton);
+        gameOverButtons.setAlignment(Pos.CENTER);
+
+        VBox gameOverContainer = new VBox(imageHBox, gameOverButtons);
+        gameOverContainer.setAlignment(Pos.CENTER);
+
+        root.setCenter(gameOverContainer);
+        root.setBackground(new Background(new BackgroundFill(Color.DARKSLATEGRAY, null, null)));
+    }
     public void startGame() {
         GameEnv env = new GameEnv();
         root.setCenter(env.render());
